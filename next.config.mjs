64b3081b16078000461d2f519bba3fe28391090f
Florian <heysen.florian@gmail.com>
2024-10-10 19:51:28 +0000

    wip safe action

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn-dev-uploads-mdsbmxre.s3.us-east-1.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "v0.dev",
-        port: "",
+        hostname: "*.s3.us-east-1.amazonaws.com",
         pathname: "/**",
       },
     ],
